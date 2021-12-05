9b44eaedf317360079c63ef0b1b052919113eb2a
Megharth <m.c.lakhataria@gmail.com>
2021-12-05 11:53:57 -0500

    add search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,13 @@
+import { Logout, Search } from "@mui/icons-material";
+import Close from "@mui/icons-material/Close";
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React, { useState, useEffect } from "react";
 
 import "../css/Home.css";
@@ -5,18 +15,68 @@ import ProductCard from "./ProductCard";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}product`).then((response) => {
-      response.json().then((data) => setProducts(data.products));
+      response.json().then((data) => {
+        setProducts(data.products);
+        setFilteredProducts(data.products);
+      });
     });
   }, []);
 
+  const handleSearch = (e) => setSearch(e.target.value);
+
+  const searchProducts = () => {
+    if (search.length > 0) {
+      setFilteredProducts(
+        products.filter((product) => product.name.includes(search))
+      );
+    } else {
+      setFilteredProducts(products);
+    }
+  };
+
   return (
     <div className="home">
+      <AppBar>
+        <Toolbar>
+          <Typography variant="body1">E-Commerce website</Typography>
+          <Paper className="search-bar">
+            <InputBase
+              sx={{ ml: 1, flex: 1, width: "500px" }}
+              fullWidth
+              placeholder="Search products"
+              onChange={handleSearch}
+              value={search}
+              onKeyPress={(e) => {
+                if (e.code === "Enter") searchProducts();
+              }}
+            />
+            {search.length > 0 ? (
+              <IconButton
+                onClick={() => {
+                  setSearch("");
+                  setFilteredProducts(products);
+                }}
+              >
+                <Close />
+              </IconButton>
+            ) : (
+              <IconButton onClick={searchProducts}>
+                <Search />
+              </IconButton>
+            )}
+          </Paper>
+          <IconButton className="logout-btn">
+            <Logout />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <div className="catalogue">
-        {products.length > 0 ? (
-          products.map((product) => (
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))
         ) : (
